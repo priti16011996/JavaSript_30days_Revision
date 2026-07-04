d1ccc1bd53287dc725bd4ba8ecaf99629ce587d3
Priti <priti16maurya@gmail.com>
2026-07-05 00:00:31 +0530

    kadanes algorithm

---
+++ MaximumSubAraySum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray(nums) {
+  let currentSum = nums[0];
+  let maxSum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
